d7713f2a5615194ab459f563570bcbad1d7d700f
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2022-03-15 11:33:31 +0800

    [fix]积木拼接方式组织文章

--- 004-platforms/001-byr-bbs/003-bytedance/002-bbs-raw/build.sh
+++ 004-platforms/001-byr-bbs/003-bytedance/002-bbs-raw/build.sh
@@ -1,1 +1,51 @@
-cat ./text.txt | pbcopy
+#!/bin/bash
+
+#todo: install gnu-sed first
+
+# cat ./text.txt | pbcopy
+touch oo.txt 
+cat ./empty.txt >& oo.txt
+
+gsed -i "1 r ./public-wechat-pic-pos.txt" oo.txt
+gsed -i "1 r ../../../../common/wechat_public.txt" oo.txt 
+
+gsed -i "1 r ./group-wechat-pic-pos.txt" oo.txt
+gsed -i "1 r ./group-wechat.txt" oo.txt
+
+
+gsed -i "1 r ./my-wechat-pic-pos.txt" oo.txt
+gsed -i "1 r ../../../../common/my_addr.txt" oo.txt 
+
+
+gsed -i "1 r ./social-ref-pic-pos.txt.txt" oo.txt
+gsed -i "1 r ./social-ref.txt" oo.txt
+
+
+gsed -i "1 r ./daily-inter-ref-pic-pos.txt" oo.txt
+gsed -i "1 r ./daily-inter-ref.txt" oo.txt
+
+
+gsed -i "1 r ./compus-ref-pic-pos.txt" oo.txt
+gsed -i "1 r ./compus-ref.txt" oo.txt
+
+
+gsed -i "1 r ./rush-ref-pic-pos.txt" oo.txt
+gsed -i "1 r ./rush-ref.txt" oo.txt
+
+
+gsed -i "1 r ./lightning-inter-ref-pic-pos.txt" oo.txt
+gsed -i "1 r ./lightning-inter-ref.txt" oo.txt
+
+
+gsed -i "1 r welfare.txt" oo.txt
+
+
+gsed -i "1 r ../../../../common/ref-result-pic-pos.txt" oo.txt
+gsed -i "1 r ../../../../common/my_intro.txt" oo.txt 
+gsed -i "1 r ../../../../common/header.txt" oo.txt 
+
+touch text.txt && cat oo.txt >& text.txt
+cat text.txt
+cat ./oo.txt | pbcopy
+rm -rf oo.txt
+
